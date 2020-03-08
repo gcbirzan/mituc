b2efc2034e10b08cf75b4342072ff7dd68b93a09
George-Cristian Bîrzan <gc@birzan.org>
2020-03-08 17:08:31 +0200

    changes

--- plugins/Mituc/plugin.py
+++ plugins/Mituc/plugin.py
@@ -7,6 +7,7 @@
 import pathlib
 import random
 import re
+import time
 
 import markovify
 from supybot import utils, plugins, ircutils, callbacks, world
@@ -29,10 +30,14 @@ class Mituc(callbacks.Plugin):
         self.network = irc.network
         self.schedule = schedule.schedule
         current_path = pathlib.Path(__file__).parent.absolute()
-        with open(current_path / "corpus.txt") as f:
+        with open(current_path / "corpus.txt", encoding='utf-8') as f:
             text = f.read()
 
         self.text_model = markovify.NewlineText(text, state_size=3)
+        with open(current_path / "corpus_bigger.txt", encoding='utf-8') as f:
+            text = f.read()
+
+        self.text_model_bigger = markovify.NewlineText(text, state_size=3)
         try:
             schedule.removeEvent('mituc')
         except:
@@ -52,27 +57,40 @@ class Mituc(callbacks.Plugin):
         except Exception as e:
             print(e)
 
-    def generate_mituc(self, text=None):
+    def generate_mituc(self, text=None, model=None):
+        if model is None:
+            model = self.text_model
+        if text:
+            words = text.lower().split()
+        start_t = time.time()
         for x in range(2000):
-            sentence = self.text_model.make_sentence(tries=150)
+            sentence = model.make_sentence(tries=150)
             sentence = re.sub('[^ ]*?:? *', '', sentence)
 
             match = True
 
             if text:
                 lower_sentence = sentence.lower()
-                words = text.lower().split()
                 for word in words:
-                    if word not in sentence:
+                    if word not in lower_sentence:
                         match = False
                         break
             if match:
-                return sentence
+                return f'{sentence}' # {x} {((time.time() - start_t) * 1000) / x}'
 
     @wrap([optional('text', )])
     def mituc(self, irc, msg, args, text):
         """mituc"""
         sentence = self.generate_mituc(text)
+        if sentence:
+            irc.reply(sentence, prefixNick=False)
+        else:
+            irc.reply("Could not generate a Mituc")
+
+    @wrap([optional('text', )])
+    def mituc2(self, irc, msg, args, text):
+        """mituc 2"""
+        sentence = self.generate_mituc(text, self.text_model_bigger)
         if sentence:
             irc.reply(sentence, prefixNick=False)
         else:
--- test.py
+++ test.py
@@ -2,15 +2,27 @@
 import re
 
 import markovify
+#
+# with open("/home/gcbirzan/PycharmProjects/mituc/plugins/Mituc/corpus.txt") as f:
+#     text = f.read()
+#
+# # Build the model.
+# text_model = markovify.NewlineText(text, state_size=3)
+#
+# # Print five randomly-generated sentences
+# for i in range(5):
+#     sentence = text_model.make_sentence(tries=150)
+#     sentence = re.sub('[^ ]*?:? *', '', sentence)
+#     print(sentence)
 
-with open("/home/gcbirzan/PycharmProjects/mituc/plugins/Mituc/corpus.txt") as f:
-    text = f.read()
+from textgenrnn import textgenrnn
+from textgenrnn.utils import textgenrnn_texts_from_file
 
-# Build the model.
-text_model = markovify.NewlineText(text, state_size=3)
+textgen = textgenrnn()
+file_path = r'C:\Users\gc\PycharmProjects\mituc\plugins\Mituc\corpus.txt'
+# file_path = r'C:\Users\gc\PycharmProjects\mituc\plugins\Mituc\corpus_bigger.txt'
+textgen.train_from_file(file_path, num_epochs=100, train_size=0.95, batch_size=1024 * 9)
+# textgen.train_from_file(file_path, num_epochs=10, train_size=0.95, batch_size=2048, name='mituc', new_model=True, rnn_layers=3, rnn_size=128)
+# textgen.train_from_file(r'C:\Users\gc\PycharmProjects\mituc\plugins\Mituc\corpus.txt', num_epochs=1, batch_size=2048 * 4)
 
-# Print five randomly-generated sentences
-for i in range(5):
-    sentence = text_model.make_sentence(tries=150)
-    sentence = re.sub('[^ ]*?:? *', '', sentence)
-    print(sentence)
+textgen.generate()
